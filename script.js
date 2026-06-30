ea05e79be82a2b6d3c1f8baf37dc152535f7fadd
matheusfigueiredosilva-droid <matheus.figueiredo.silva@escola.pr.gov.br>
2026-06-30 07:55:10 -0300

    Add function to display a message about agriculture

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem(){
+
+    document.getElementById("mensagem").innerHTML =
+    "Você sabia? Cerca de 70% dos alimentos consumidos pelos brasileiros vêm da agricultura familiar, mostrando como o campo é essencial para nossa alimentação.";
+
+}
